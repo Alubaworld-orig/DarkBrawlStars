ddd2e28b480387d6c6ec476ba7cd6d5d5a21e7a5
Alubaworld-orig <aluba2614@gmail.com>
2026-03-18 12:59:03 +0100

    Create CharacterManager.js with dark scary characters

---
+++ src/characters/CharacterManager.js
@@ -0,0 +1,31 @@
+// CharacterManager.js
+
+const darkScaryCharacters = [
+    {
+        name: "Nightmare",
+        power: "Shadow Manipulation",
+        description: "Controls shadows to terrify enemies."
+    },
+    {
+        name: "Wraith",
+        power: "Invisibility",
+        description: "A ghostly figure that can appear and disappear at will."
+    },
+    {
+        name: "Grim Reaper",
+        power: "Soul Harvesting",
+        description: "A harbinger of death who collects souls."
+    },
+    {
+        name: "Vampire Lord",
+        power: "Blood Siphon",
+        description: "Drains the life force from its victims."
+    },
+    {
+        name: "Werewolf",
+        power: "Ferocity",
+        description: "A fierce creature that transforms with the full moon."
+    }
+];
+
+export default darkScaryCharacters;
